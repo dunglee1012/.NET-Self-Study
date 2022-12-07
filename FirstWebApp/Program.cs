cf305d6d05ece58facb3dc1d1e77b10b2c6600c1
LeNguyenQuangDung <leenguyenquangdung1@gmail.com>
2022-12-07 10:32:02 +0700

    update

--- FirstWebApp/Program.cs
+++ FirstWebApp/Program.cs
@@ -13,7 +13,7 @@ internal class Program
             );
         app.MapControllerRoute(
            name: "default",
-           pattern: "/{controller=Product}/{action=Index}/{id}/{name}"
+           pattern: "/{controller=Product}/{action=Index}/{id}/{name=ABC}"
            );
 
         //app.MapGet("/", () => "Hello World!");
